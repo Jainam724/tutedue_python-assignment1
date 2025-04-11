36be227531674403dc255a97dab155b69bef9c01
Jainam Gandhi <103090341+Jainam724@users.noreply.github.com>
2025-04-11 19:07:30 +0530

    Add files via upload

---
+++ assignment1-task1.py
@@ -0,0 +1,13 @@
+a = int(input("Enetr first number:"))
+b = int(input("Enetr second number:"))
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print()
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
